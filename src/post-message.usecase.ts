9f82a7154bf2dd4614bb2eafaf4c618d3e51ea72
Malek Rezgui <m.rezgui@outlook.fr>
2025-02-24 11:48:36 +0100

    create fixture and implement empty message tests

--- src/post-message.usecase.ts
+++ src/post-message.usecase.ts
@@ -19,6 +19,9 @@ export interface DateProvider {
   getNow(): Date;
 }
 
+export class MessageTooLongError extends Error {}
+export class EmptyMessageError extends Error {}
+
 export class PostMessageUseCase {
   constructor(
     private readonly messageRepository: MessageRepository,
@@ -26,6 +29,14 @@ export class PostMessageUseCase {
   ) {}
 
   handle(postMessageCommand: PostMessageCommand) {
+    if (postMessageCommand.text.length > 280) {
+      throw new MessageTooLongError();
+    }
+
+    if (postMessageCommand.text.trim().length === 0) {
+      throw new EmptyMessageError();
+    }
+
     this.messageRepository.save({
       id: postMessageCommand.id,
       text: postMessageCommand.text,
--- src/tests/posting-message.spec.ts
+++ src/tests/posting-message.spec.ts
@@ -1,37 +1,84 @@
 import {
   DateProvider,
+  EmptyMessageError,
   Message,
   MessageRepository,
+  MessageTooLongError,
   PostMessageCommand,
   PostMessageUseCase,
 } from "../post-message.usecase";
 
 describe("Feature : Posting a message", () => {
+  let fixture: Fixture;
+
+  beforeEach(() => {
+    fixture = createFixture();
+  });
+
   describe("Rule : A message can contain a maximun of 280 characters", () => {
     test("Alice can post a message on her timeline", () => {
-      giveNowIs(new Date("2025-02-23T19:00:00.000Z"));
+      fixture.giveNowIs(new Date("2025-02-23T19:00:00.000Z"));
 
-      whenUserPostAmessage({
+      fixture.whenUserPostAmessage({
         id: "message-id",
         text: "Hello World",
         author: "Alice",
       });
 
-      thenPostedMessageShouldBe({
+      fixture.thenPostedMessageShouldBe({
         id: "message-id",
         text: "Hello World",
         author: "Alice",
         publishedAt: new Date("2025-02-23T19:00:00.000Z"),
       });
     });
+
+    test("Alice cannot post a message with more than 280 characters", () => {
+      const textWith281Char =
+        "Twitter naît en 2006, créé par Jack Dorsey, Noah Glass, Biz Stone et Evan Williams. Projet interne d’Odeo, il devient une plateforme publique. Les tweets, limités à 140 caractères, révolutionnent la communication en ligne par leur rapidité et simplicité, marquant une nouvelle ère numérique.";
+      fixture.giveNowIs(new Date("2025-02-23T19:00:00.000Z"));
+
+      fixture.whenUserPostAmessage({
+        id: "message-id",
+        author: "Alice",
+        text: textWith281Char,
+      });
+
+      fixture.thenErrorShouldbe(MessageTooLongError);
+    });
+  });
+
+  describe("Rule : A message cannot be empty", () => {
+    test("Alice cannot post an empty message", () => {
+      fixture.giveNowIs(new Date("2025-02-23T19:00:00.000Z"));
+
+      fixture.whenUserPostAmessage({
+        id: "message-id",
+        author: "Alice",
+        text: "",
+      });
+
+      fixture.thenErrorShouldbe(EmptyMessageError);
+    });
+
+    test("Alice cannot post a message with only whitespaces", () => {
+      fixture.giveNowIs(new Date("2025-02-23T19:00:00.000Z"));
+
+      fixture.whenUserPostAmessage({
+        id: "message-id",
+        author: "Alice",
+        text: "      ",
+      });
+
+      fixture.thenErrorShouldbe(EmptyMessageError);
+    });
   });
 });
 
-let message: Message;
-
 class InMemoryMessageRepository implements MessageRepository {
+  message: Message;
   save(msg: Message): void {
-    message = msg;
+    this.message = msg;
   }
 }
 
@@ -42,22 +89,33 @@ class StubDateProvider implements DateProvider {
   }
 }
 
-const messageRepository = new InMemoryMessageRepository();
-const dateProvider = new StubDateProvider();
+function createFixture() {
+  const dateProvider = new StubDateProvider();
+  const messageRepository = new InMemoryMessageRepository();
+  const postMessageUseCase = new PostMessageUseCase(
+    messageRepository,
+    dateProvider
+  );
+  let thrownError: Error;
 
-const postMessageUseCase = new PostMessageUseCase(
-  messageRepository,
-  dateProvider
-);
-
-function giveNowIs(_now: Date) {
-  dateProvider.now = _now;
+  return {
+    giveNowIs(now: Date) {
+      dateProvider.now = now;
+    },
+    whenUserPostAmessage(postMessageCommand: PostMessageCommand) {
+      try {
+        postMessageUseCase.handle(postMessageCommand);
+      } catch (err) {
+        thrownError = err;
+      }
+    },
+    thenPostedMessageShouldBe(expectedMessage: Message) {
+      expect(expectedMessage).toEqual(messageRepository.message);
+    },
+    thenErrorShouldbe(expectedErrorClass: new () => Error) {
+      expect(thrownError).toBeInstanceOf(expectedErrorClass);
+    },
+  };
 }
 
-function whenUserPostAmessage(postMessageCommand: PostMessageCommand) {
-  postMessageUseCase.handle(postMessageCommand);
-}
-
-function thenPostedMessageShouldBe(expectedMessage: Message) {
-  expect(expectedMessage).toEqual(message);
-}
+type Fixture = ReturnType<typeof createFixture>;
