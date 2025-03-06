0edc397c5e9b7fcd674e19d5dc2f0a179207ada4
Malek Rezgui <m.rezgui@outlook.fr>
2025-03-06 14:34:31 +0100

    add integration tests for FollowUser and ViewWall use cases

--- src/infra/FileSystemFolloweeRepository.ts
+++ src/infra/FileSystemFolloweeRepository.ts
@@ -1,7 +1,31 @@
-import * as fs from "fs";
 import * as path from "path";
-import { FolloweeRepository } from "../application/FolloweeRepository";
+import * as fs from "fs";
+import {
+  Followee,
+  FolloweeRepository,
+} from "../application/FolloweeRepository";
 
 export class FileSystemFolloweeRepository implements FolloweeRepository {
-  constructor(private readonly followe) {}
+  constructor(
+    private readonly followeesPath = path.join(__dirname, "./followees.json")
+  ) {}
+
+  async saveFollowee({ user, followee }: Followee): Promise<void> {
+    const followees = await this.getFollowees();
+    const actualUserFollowees = followees[user] ?? [];
+    actualUserFollowees.push(followee);
+    followees[user] = actualUserFollowees;
+
+    return fs.promises.writeFile(this.followeesPath, JSON.stringify(followees));
+  }
+
+  async getFolloweesOf(user: string): Promise<string[]> {
+    const allFollowees = await this.getFollowees();
+    return allFollowees[user];
+  }
+
+  private async getFollowees() {
+    const data = await fs.promises.readFile(this.followeesPath);
+    return JSON.parse(data.toString()) as { [user: string]: string[] };
+  }
 }
--- src/infra/tests/FileSystemMessageRepository.integration.test.ts
+++ src/infra/tests/FileSystemMessageRepository.integration.test.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 import * as fs from "fs";
-import { messageBuilder } from "./MessageBuilder";
-import { FileSystemMessageRepository } from "../infra/FileSystemMessageRepository";
+import { messageBuilder } from "../../tests/MessageBuilder";
+import { FileSystemMessageRepository } from "../FileSystemMessageRepository";
 
 const testMessagePath = path.join(__dirname, "./messages-test.json");
 
